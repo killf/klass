b89a3df87c45adcfe911d79d556f35de3bc32c67
killf <killf@foxmail.com>
2020-09-30 16:05:25 +0800

    update

--- data/cifar.py
+++ data/cifar.py
@@ -1,6 +1,6 @@
 import os
 
-__all__ = ["CIFAR10"]
+__all__ = ["CIFAR10", "CIFAR100"]
 
 
 class CIFAR10:
@@ -14,6 +14,26 @@ class CIFAR10:
         self.train_set = cifar.CIFAR10(args.get("train_dir", "data_dir", default_root), train=True, download=True)
         self.val_set = cifar.CIFAR10(args.get("val_dir", "data_dir", default_root), train=False, download=True)
         self.test_set = cifar.CIFAR10(args.get("test_dir", "data_dir", default_root), train=False, download=True)
+
+        self.train_loader = DataLoader(self.train_set, batch_size=args.get("train_batch_size", "batch_size", 1))
+        self.val_loader = DataLoader(self.val_set, batch_size=args.get("val_batch_size", "batch_size", 1))
+        self.test_loader = DataLoader(self.test_set, batch_size=args.get("test_batch_size", "batch_size", 1))
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+
+class CIFAR100:
+    def __init__(self, args):
+        super(CIFAR100, self).__init__()
+
+        from torch.utils.data import DataLoader
+        from torchvision.datasets import cifar
+
+        default_root = os.path.join(os.path.expanduser('~'), ".klass/data/cifar100")
+        self.train_set = cifar.CIFAR100(args.get("train_dir", "data_dir", default_root), train=True, download=True)
+        self.val_set = cifar.CIFAR100(args.get("val_dir", "data_dir", default_root), train=False, download=True)
+        self.test_set = cifar.CIFAR100(args.get("test_dir", "data_dir", default_root), train=False, download=True)
 
         self.train_loader = DataLoader(self.train_set, batch_size=args.get("train_batch_size", "batch_size", 1))
         self.val_loader = DataLoader(self.val_set, batch_size=args.get("val_batch_size", "batch_size", 1))
